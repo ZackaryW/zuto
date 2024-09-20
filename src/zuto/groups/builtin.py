5fb2634c9ca7adc72d85e31e617a90f735abdab1
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-09-20 13:17:43 -0700

    fix: lifetime handle

--- src/zuto/groups/builtin.py
+++ src/zuto/groups/builtin.py
@@ -1,10 +1,12 @@
 import os
 import threading
+from time import sleep
+import typing
 from ..utils import resolve_auto, splitstring
 from ..runner import ZutoCtx
 from ..group import ZutoGroup
 from zuu.stdpkg.subprocess import open_detached
-from zuu.stdpkg.time import sleep_until
+from zuu.stdpkg.time import sleep_until, remaining_time
 import pygetwindow as gw
 builtin = ZutoGroup("builtin")
 
@@ -47,17 +49,17 @@ def _eval(ctx : ZutoCtx, code : str):
     varname = code.split("=")[0].strip()
     return ctx.env[varname]
 
-@builtin.cmd("exec", resolve_strings=False)
-def _exec(_val: str):
-    w = splitstring(_val)
+@builtin.cmd("exec")
+def _exec(cmd: str):
+    w = splitstring(cmd)
     open_detached(*w)
 
-@builtin.cmd("cmd")
-def cmd(_val : str):
-    os.system(_val)
+@builtin.cmd("os")
+def _os(val : str):
+    os.system(val)
 
-@builtin.cmd()
-def sleep(until: str):
+@builtin.cmd("sleep")
+def _sleep(until: str):
     sleep_until(until)
 
 @builtin.handler("*")
@@ -71,31 +73,20 @@ def lifetime_pre_handle(ctx : ZutoCtx, state : str):
     if "lifetime" not in ctx.cmd:
         return
     
-    ctx.meta["windows1"] = gw.getAllWindows()
-
-@builtin.handler("*")
-def lifetime_after_handle(ctx : ZutoCtx, state : str):
-    if state != "after":
-        return
-
-    if "windows1" not in ctx.meta:
-        return
+    ctx.meta["windows1"] = {w._hWnd : w for w in gw.getAllWindows() if w.title}
     
-    
-    windows1 = ctx.meta["windows1"]
-    windows1d = {w.title: w for w in windows1}
+
+@builtin.cmd()
+def lifetime(ctx : ZutoCtx, until : str |int):
     sleep(1)
-    windows2 = gw.getAllWindows()
-    remainder_time = ctx.meta["remainder_time"]
-
-    
     def target():
-        
-        differedWnds = [
-            w for w in windows2 
-            if w.title and w.title not in windows1d 
-            and w not in windows1d.values()
-        ]
+        remainder_time = remaining_time(until)
+        windows1 : typing.Dict[str, gw.Win32Window] = ctx.meta["windows1"].copy()
+        sleep(0.5)
+        windows2 = {w._hWnd : w for w in gw.getAllWindows() if w.title}
+        differedWnds = set(windows2.keys()) - set(windows1.keys())
+        differedWnds = [windows2[k] for k in differedWnds]
+
         if len(differedWnds) == 0:
             return
         print(f"scheduled to kill {len(differedWnds)} windows ({differedWnds[0].title}...)")
@@ -109,8 +100,3 @@ def lifetime_after_handle(ctx : ZutoCtx, state : str):
     thread = threading.Thread(target=target)
     thread.daemon = True
     thread.start()
-
-@builtin.cmd()
-def lifetime(ctx : ZutoCtx, until : str |int):
-    ctx.parentMeta["remainder_time"] = until
-    ctx.parentMeta["windows2"] = gw.getAllWindows()
